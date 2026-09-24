62d54e86dc6bc30202b0888d46d9829fd19398ec
arihant9211 <70895985+arihant9211@users.noreply.github.com>
2026-09-25 03:03:19 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        dict1  = {}
+
+        for i in s:
+            if i in dict1:
+                dict1[i] = dict1[i] + 1
+            else:
+                dict1.update({i: 1})
+        dict2 = {}
+
+        for i in t:
+            if i in dict2:
+                dict2[i] = dict2[i] + 1
+            else:
+                dict2.update({i: 1})
+        print(dict1)
+        print(dict2)
+        return (dict1 == dict2)
